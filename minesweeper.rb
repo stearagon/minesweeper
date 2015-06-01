aba24db4655115b6f9440e29f625b898923d6712
Peter+Steve <peter+steve@appacademy.io>
2015-06-01 15:09:35 -0400

    Fix user input for move method

--- minesweeper.rb
+++ minesweeper.rb
@@ -28,8 +28,7 @@ class Board
     @grid = grid
   end
 
-  def [](pos)
-    row, col = pos[0], pos[1]
+  def [](row,col)
     self.grid[row][col]
   end
 
@@ -73,18 +72,59 @@ end
 
 class Game
 
-  def play
+  def initialize(board)
+    @board = board
+  end
 
+  def self.play
+    #creates game board with input from user for number of bombs
     puts "Enter number of bombs."
 
 
-    board = Board.seed(gets.chomp.to_i)
-    
-    board.display
 
+    new_game = Game.new(Board.seed(gets.chomp.to_i))
 
+    until new_game.over?
+      @board.display
+      new_game.move
+
+    end
+
+    if win?
+      puts "You win!"
+    else
+      puts "You lose"
+    end
 
   end
+
+  def over?
+    win? || lose?
+    #user picks tile that is bomb, or user reveals all other tiles
+  end
+
+  def lose?
+    false
+
+  end
+
+  def win?
+    false
+  end
+
+  def move
+    puts "Select tile with r/f and position"
+    input = gets.chomp.split("")
+    row, col = input[1].to_i, input[2].to_i
+    if input[0] == "r"
+      #reveal tile at specified location
+      @board[row,col].revealed == true
+    elsif input[0] == "f"
+      #flag tile at location
+      @board[row,col].flagged == true
+    end
+  end
+
 
 
 end
