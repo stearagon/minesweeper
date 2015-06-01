5ba8690f66e79d1e6ded404a30a49ce78a0b3ae5
Peter+Steve <peter+steve@appacademy.io>
2015-06-01 11:39:44 -0400

    Add bomb seeding functionality to seed, create Tile.initialize method.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,19 +1,25 @@
 class Board
+  BOARD_SIZE = 9
+
 
   attr_accessor :board
   def initialize(board)
     @board = board
-
   end
 
   def self.seed(bombs)
 
-    board = Array.new { [] }
+    board = Array.new(9) { Array.new(9) { "_" } }
 
+    bombs.times do |x|
+      row, col = rand(0...BOARD_SIZE), rand(0...BOARD_SIZE)
+      
+    end
 
     Board.new(board)
 
   end
+
 
 
 
