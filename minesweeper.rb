871a01bac1cee40855407ba701fcf9411ba03829
Peter+Steve <peter+steve@appacademy.io>
2015-06-01 12:38:11 -0400

    Edit tile initialize, add helper to board for position.

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,11 +2,7 @@ require 'byebug'
 class Board
   BOARD_SIZE = 9
 
-
   attr_accessor :board
-  def initialize(board)
-    @board = board
-  end
 
   #Factory method to create new board filled with "bombs" number of bombs
   def self.seed(bombs)
@@ -17,8 +13,8 @@ class Board
     until idx == bombs do
       row, col = rand(0...BOARD_SIZE), rand(0...BOARD_SIZE)
       current_tile = board[row][col]
-      unless current_tile.type == :bomb
-        current_tile.type = :bomb
+      unless current_tile.bomb == true
+        current_tile.bomb = true
         idx += 1
       end
     end
@@ -27,9 +23,22 @@ class Board
 
   end
 
+  def initialize(board)
+    @board = board
+  end
+
+  def [](pos)
+    row, col = pos[0], pos[1]
+    @board[row][col]
+  end
+  
+  def []=(row,col)
+
+  end
+
+
   #Displays board by showing each tile's display_value
   def display
-    debugger
     self.board.map do |row|
         row.map do |tile|
          tile.display_value
@@ -37,50 +46,26 @@ class Board
     end
   end
 
-
 end
-
-
-
-
-
-
-
-
-
-
-
 
 
 class Tile
 
-  attr_accessor :type, :display_value, :flagged, :revealed
+  attr_accessor :bomb, :display_value, :flagged, :revealed
+
   def initialize(
-                  type = nil,
-                  display_value = "_",
+                  bomb = false,
                   flagged = false,
                   revealed = false
                 )
-    @type = type
-    @display_value = display_value
+    @bomb = bomb
+    @display_value = "_"
     @flagged = flagged
     @reveled = revealed
   end
 
 
-
-
 end
-
-
-
-
-
-
-
-
-
-
 
 
 
